4ac0ea7de58087ddb9254b3043c46c2b341542f6
Jay Windley <jay.windley@bluecoat.com>
2015-06-11 13:13:56 -0600

    setup shell script

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HERE=${PWD}
+
+# Follow user's predetermined method of backups.
+export VERSION_CONTROL=existing
+
+alias make_link='ln -f -s -b'
+
+# Shell / Bash
+echo -n "Setting up Bash and shell..."
+make_link $HERE/bash/bashrc $HOME/.bashrc
+make_link $HERE/bash/dircolors $HOME/.dircolors
+echo "done"
+
+# Fonts
+echo -n "Setting up fonts..."
+mkdir -p $HOME/.fonts
+echo -n "deferring font population..."
+echo "done"
+
+# Emacs
+echo -n "Setting up Emacs..."
+emacs_dir=$HOME/.emacs.d
+mkdir -p $emacs_dir
+make_link $HERE/emacs/init.el $emacs_dir/init.el
+make_link $HERE/emacs/jay $emacs_dir/jay
+echo "done"
+
+exit 0
+
